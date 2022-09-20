57340446264dd97d481fd53f79b99562d478d9c7
ckmine <36189229+ckmine@users.noreply.github.com>
2022-09-20 15:18:27 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-sudo cd /var/lib/jenkins/workspace/mine-project/
+cd /var/lib/jenkins/workspace/mine-project/
 su jenkins
 #docker logout   master.mine.com
 sudo docker login -u admin --password-stdin admin@123 master.mine.com
-sudo docker image build -t  $JOB_NAME:v1.$BUILD_ID .
-sudo docker image tag $JOB_NAME:v1.$BUILD_ID master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
-sudo docker image tag $JOB_NAME:v1.$BUILD_ID master.mine.com/holder/$JOB_NAME:latest
-sudo docker image push master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
-sudo docker  push master.mine.com/holder/$JOB_NAME:latest
-sudo docker image rmi $JOB_NAME:v1.$BUILD_ID  master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID   master.mine.com/holder/$JOB_NAME:latest 
+ docker image build -t  $JOB_NAME:v1.$BUILD_ID .
+ docker image tag $JOB_NAME:v1.$BUILD_ID master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
+ docker image tag $JOB_NAME:v1.$BUILD_ID master.mine.com/holder/$JOB_NAME:latest
+ docker image push master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
+ docker  push master.mine.com/holder/$JOB_NAME:latest
+ docker image rmi $JOB_NAME:v1.$BUILD_ID  master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID   master.mine.com/holder/$JOB_NAME:latest 
