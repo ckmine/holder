f3c42f85cba70dd0508c4297b1b900a3d11a58f4
ckmine <36189229+ckmine@users.noreply.github.com>
2022-09-21 16:47:04 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -8,4 +8,4 @@ sudo  docker image build -t  $JOB_NAME:v1.$BUILD_ID .
  docker image tag $JOB_NAME:v1.$BUILD_ID master.mine.com/holder/$JOB_NAME:latest
  docker image push master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
  docker  push master.mine.com/holder/$JOB_NAME:latest
- docker image rmi $JOB_NAME:v1.$BUILD_ID  master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID   master.mine.com/holder/$JOB_NAME:latest 
+ #docker image rmi $JOB_NAME:v1.$BUILD_ID  master.mine.com/holder/$JOB_NAME:v1.$BUILD_ID   master.mine.com/holder/$JOB_NAME:latest 
