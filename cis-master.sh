5204eaf183ab0f5466232e31a3710f7eddf4e041
ckmine <36189229+ckmine@users.noreply.github.com>
2022-11-06 20:12:29 +0530

    Update cis-master.sh

--- cis-master.sh
+++ cis-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #cis-master.sh
 
-total_fail=$(kube-bench master  --version 1.25 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
+total_fail=$(kube-bench master  --version 1.25 --check 1.2.7,1.2.8,1.2.9 --json )
 
 if [[ "$total_fail" -ne 0 ]];
         then
