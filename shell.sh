635b966c3944fb26fe048408e1463d2936059486
ckmine <36189229+ckmine@users.noreply.github.com>
2023-02-15 23:08:45 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@
 cd /var/lib/jenkins/workspace/mine-project/
 sudo su - jenkins -s/bin/bash
 #docker logout   masternode.mine.com
- docker login -u admin --password-stdin Harbor12345 masternode.mine.com
+ sudo docker login -u admin --password-stdin Harbor12345 masternode.mine.com
 sudo  docker image build -t  $JOB_NAME:v1.$BUILD_ID .
  docker image tag $JOB_NAME:v1.$BUILD_ID masternode.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
  docker image tag $JOB_NAME:v1.$BUILD_ID mastenoder.mine.com/holder/$JOB_NAME:latest
