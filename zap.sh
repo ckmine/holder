bb3047d2e88db52d47efc086a2cc60281e361a22
ckmine <36189229+ckmine@users.noreply.github.com>
2023-02-16 12:54:10 +0530

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -15,8 +15,8 @@ exit_code=$?
 
 
 # HTML Report
- sudo mkdir -p owasp-zap-report
- sudo mv zap_report.html owasp-zap-report
+  mkdir -p owasp-zap-report
+  mv zap_report.html owasp-zap-report
 
 
 echo "Exit Code : $exit_code"
